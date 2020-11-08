44f10c62250af5ae94e03d6cf5700bd93a105bcd
DesireeJoy <desireebradish@gmail.com>
2020-11-07 22:18:47 -0500

    To show Jess

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+const editProfileForm = document.querySelector(".popup");
+
+function editProfile() {
+  alert("nope");
+  editProfileForm.setAttribute("visibility", visible);
+}
+
+editProfileForm.addEventListener("click", editProfile);
