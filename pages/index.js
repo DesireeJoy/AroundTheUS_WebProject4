8d00cb9cebe53673269b8bf74a5591f318363b70
DesireeJoy <desireebradish@gmail.com>
2020-11-13 19:06:12 -0500

    Fix popup to use modifiers for inputs and change to SVG image type for buttons

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,10 @@ let popUp = document.querySelector(".popup");
 
 //Show and Hide
 function editProfile() {
-  popUp.classList.add("popup__view-visible");
+  popUp.classList.add("popup_visible");
 }
 function closeEdit() {
-  popUp.classList.remove("popup__view-visible");
+  popUp.classList.remove("popup_visible");
 }
 
 // Let's find the form in the DOM
@@ -20,14 +20,14 @@ function formSubmitHandler(evt) {
   // This line stops the browser from submitting the form in the default way.
 
   // Let's find the form fields in the DOM // Get the corresponding Value
-  let newName = document.querySelector(".popup__name").value; // Use querySelector()
+  let newName = document.querySelector(".popup__input").value; // Use querySelector()
   let newTitle = document.querySelector(".popup__title").value; // Use querySelector()
 
   // Insert new values using the textContent property of the
   // querySelector() method
   document.querySelector(".profile__name-span").textContent = newName;
   document.querySelector(".profile__title").textContent = newTitle;
-  popUp.classList.remove("popup__view-visible");
+  popUp.classList.remove("popup_visible");
 }
 
 // Connect the handler to the form:
