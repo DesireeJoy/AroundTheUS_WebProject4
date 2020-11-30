c0b4372291f969210ff63750ded65ed2d9096a04
DesireeJoy <desireebradish@gmail.com>
2020-11-29 19:34:33 -0500

    Final changes done

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,7 @@
-//Initial Array
-
-const initialCards = [
-  {
-    name: "Ecola State Park",
-    link: "images/EcolaStatePark.jpeg",
-    alt: "Tall trees shrouded in mist on a hillside",
-  },
-  {
-    name: "Fontainebleau State Park",
-    link: "images/FontainebleauStatePark.jpeg",
-    alt:
-      "Orange sunset over a lake with silhouetted black trees in the foreground",
-  },
-  {
-    name: "Governor Dodge State Park",
-    link: "images/GovernorDodgeStatePark.jpeg",
-    alt:
-      "An iced over lake beginning to thaw with trees beginning to grow leaves and a distanct green hill",
-  },
-  {
-    name: "Hocking Hills State Park",
-    link: "images/HockingHillsStatePark.jpeg",
-    alt:
-      "A small waterfall over a rocky outcropping with a sandy forground and a bridge in the background",
-  },
-  {
-    name: "Stone Mountain State Park",
-    link: "images/StoneMountainStatePark.jpeg",
-    alt:
-      "A hiker standing on the edge of an overhang looking out at a mountainside covered in trees and shrouded in dense fog",
-  },
-  {
-    name: "Valley of Fire State Park",
-    link: "images/ValleyOfFireStatePark.jpeg",
-    alt:
-      "an arid orange landscape dotted with short round desert plants with several chimney type rock formations",
-  },
-];
-
+"use strict";
 // Let's set all the constant we need
-const form = document.querySelector(".popup__form");
-const form2 = document.querySelector(".popup2__form");
-const closeElement = document.querySelector(".popup__close");
-const closeCard = document.querySelector(".popup2__close");
+const userForm = document.querySelector(".popup__form");
+const cardForm = document.querySelector(".popup__card_form");
 const editBtn = document.querySelector(".profile__editbtn");
 const addBtn = document.querySelector(".profile__addbtn");
 const inputName = document.querySelector("#inputName");
@@ -52,87 +11,69 @@ const currentTitle = document.querySelector(".profile__title");
 const cardTemplate = document.querySelector("#cardTemplate").content;
 const gridList = document.querySelector(".grid__list");
 const popUpProfile = document.querySelector(".popup");
-const popUpCard = document.querySelector(".popup2");
+const popUpCard = document.querySelector(".popup__card");
 
-// Add Initial Set of Cards
-for (var i = 0; i < initialCards.length; i++) {
-  // clone content of template tag for cards
-  let cardElement = cardTemplate.cloneNode(true);
-  let newName = initialCards[i].name;
-  let newImg = initialCards[i].link;
-  let newAlt = initialCards[i].alt;
-  cloneName = cardElement.querySelector(".grid__caption");
-  cloneImage = cardElement.querySelector(".grid__image");
+initialCards.forEach((element) => {
+  const cardElement = cardTemplate.cloneNode(true);
+  const newName = element.name;
+  const newImg = element.link;
+  const newAlt = element.alt;
+  const cloneName = cardElement.querySelector(".grid__caption");
+  const cloneImage = cardElement.querySelector(".grid__image");
   cloneName.innerHTML = newName;
   cloneImage.src = newImg;
-  cloneImage.alt = newAlt;
+  cloneImage.alt = newName;
 
-  //Like Callback
-  likePlace(cardElement);
-
-  //Enlarge Callback
-  enlarge(cardElement);
-
-  //Delete Callback
-  deletePlace(cardElement);
-
-  gridList.append(cardElement);
-}
-
-//Add new cards
+  addCardToDom(cardElement);
+});
 
 function addCard(evt) {
   // This line stops the browser from submitting the form in the default way.
   evt.preventDefault();
+
   //Form Values
   const inputPlace = document.querySelector("#inputPlace");
   const inputUrl = document.querySelector("#inputFile");
 
   const cardTemplate = document.querySelector("#cardTemplate").content;
-  let cardElement = cardTemplate.cloneNode(true);
+  const cardElement = cardTemplate.cloneNode(true);
 
-  cloneName = cardElement.querySelector(".grid__caption");
-  cloneImage = cardElement.querySelector(".grid__image");
+  const cloneName = cardElement.querySelector(".grid__caption");
+  const cloneImage = cardElement.querySelector(".grid__image");
 
   cloneName.innerHTML = inputPlace.value;
   cloneImage.src = inputUrl.value;
 
-  //Like Callback
-  likePlace(cardElement);
-
-  //enlarge Callback
-  enlarge(cardElement);
-
-  //Delete Callback
-  deletePlace(cardElement);
-
-  gridList.append(cardElement);
-  closeCardPopup();
-}
-
-//Show and Hide
-function openProfilePopup() {
-  popUpProfile.classList.add("popup_visible");
-  //Place current Profile Input in Fields
-  inputName.value = currentName.textContent;
-  inputTitle.value = currentTitle.textContent;
-}
-
-function closeProfilePopup() {
-  popUpProfile.classList.remove("popup_visible");
-}
-
-function openCardPopup() {
-  popUpCard.classList.add("popup_visible");
-  //Place current Profile Input in Fields
-}
-function closeCardPopup() {
+  addCardToDom(cardElement);
   popUpCard.classList.remove("popup_visible");
+}
+
+//Show and Hide all Modal Windows
+function openPopUp(evt) {
+  //Place current Profile Input in Fields
+
+  if (evt.target.name == "edit_btn") {
+    inputName.value = currentName.textContent;
+    inputTitle.value = currentTitle.textContent;
+    popUpProfile.classList.add("popup_visible");
+  }
+  if (evt.target.name == "add_btn") {
+    popUpCard.classList.add("popup_visible");
+  }
+}
+
+function closePopUp(evt) {
+  if (evt.target.name === "close_card") {
+    popUpCard.classList.remove("popup_visible");
+  }
+  if (evt.target.name === "close_profile") {
+    popUpProfile.classList.remove("popup_visible");
+  }
 }
 // Next is the form submit handler, though
 // it won't submit anywhere just yet
 
-function formSubmitHandler(evt) {
+function userFormSubmitHandler(evt) {
   // This line stops the browser from submitting the form in the default way.
   evt.preventDefault();
 
@@ -140,30 +81,34 @@ function formSubmitHandler(evt) {
   // querySelector() method
   currentName.textContent = inputName.value;
   currentTitle.textContent = inputTitle.value;
-  closeProfilePopup();
+  popUpProfile.classList.remove("popup_visible");
 }
 
 //FUNCTIONS FOR ALL PLACE CARDS
 
 //Function for Enlarge Callback
 function enlarge(ele) {
-  const picTemplate = document.querySelector("#pictureTemplate").content;
+  const popupImageBlock = document.querySelector(".popup__image");
+
   ele
     .querySelector(".grid__btn_popup")
     .addEventListener("click", function (pic) {
-      let picElement = picTemplate.cloneNode(true);
-      imgSelector = picElement.querySelector(".grid__image_active");
-      imgSelector.src = pic.target.src;
-      let popUpCont = picElement.querySelector(".popup3");
-      popUpCont.classList.add("popup_visible");
-      gridList.append(picElement);
+      popupImageBlock.classList.add("popup_visible");
 
-      let closeImage = popUpCont.querySelector(".popup3__close");
-      if (!closeImage !== true) {
-        closeImage.addEventListener("click", function (evt) {
-          popUpCont.remove();
-        });
-      }
+      const popupImage = popupImageBlock.querySelector(".grid__image_active");
+      popupImage.src = pic.target.src;
+
+      const popupCaption = popupImageBlock.querySelector(".popup__image_capt");
+      const newCaption = pic.target;
+
+      popupCaption.innerHTML = pic.target.alt;
+
+      console.log(newCaption);
+
+      const closeImage = popupImageBlock.querySelector(".popup__image_close");
+      closeImage.addEventListener("click", function (evt) {
+        popupImageBlock.classList.remove("popup_visible");
+      });
     });
 }
 
@@ -177,17 +122,31 @@ function likePlace(ele) {
 // Function for Delete Callback
 function deletePlace(ele) {
   ele.querySelector(".grid__btn_del").addEventListener("click", function (evt) {
-    item = evt.target.closest(".grid__card");
+    const item = evt.target.closest(".grid__card");
     item.remove();
   });
+}
+
+function addCardToDom(cardElement) {
+  //Like Callback
+  likePlace(cardElement);
+
+  //Enlarge Callback
+  enlarge(cardElement);
+
+  //Delete Callback
+  deletePlace(cardElement);
+
+  gridList.prepend(cardElement);
 }
 
 // Connect the handler to the form:
 // it will watch the submit event
 
-form.addEventListener("submit", formSubmitHandler);
-closeElement.addEventListener("click", closeProfilePopup);
-editBtn.addEventListener("click", openProfilePopup);
-closeCard.addEventListener("click", closeCardPopup);
+userForm.addEventListener("submit", userFormSubmitHandler);
+
+addBtn.addEventListener("click", openPopUp);
+editBtn.addEventListener("click", openPopUp);
+userForm.querySelector(".popup__close").addEventListener("click", closePopUp);
+cardForm.querySelector(".popup__close").addEventListener("click", closePopUp);
 popUpCard.addEventListener("submit", addCard);
-addBtn.addEventListener("click", openCardPopup);
---
+++ pages/initialCards.js
@@ -0,0 +1,39 @@
+//Initial Array
+
+const initialCards = [
+  {
+    name: "Ecola State Park",
+    link: "images/EcolaStatePark.jpeg",
+    alt: "Tall trees shrouded in mist on a hillside",
+  },
+  {
+    name: "Fontainebleau State Park",
+    link: "images/FontainebleauStatePark.jpeg",
+    alt:
+      "Orange sunset over a lake with silhouetted black trees in the foreground",
+  },
+  {
+    name: "Governor Dodge State Park",
+    link: "images/GovernorDodgeStatePark.jpeg",
+    alt:
+      "An iced over lake beginning to thaw with trees beginning to grow leaves and a distanct green hill",
+  },
+  {
+    name: "Hocking Hills State Park",
+    link: "images/HockingHillsStatePark.jpeg",
+    alt:
+      "A small waterfall over a rocky outcropping with a sandy forground and a bridge in the background",
+  },
+  {
+    name: "Stone Mountain State Park",
+    link: "images/StoneMountainStatePark.jpeg",
+    alt:
+      "A hiker standing on the edge of an overhang looking out at a mountainside covered in trees and shrouded in dense fog",
+  },
+  {
+    name: "Valley of Fire State Park",
+    link: "images/ValleyOfFireStatePark.jpeg",
+    alt:
+      "an arid orange landscape dotted with short round desert plants with several chimney type rock formations",
+  },
+];
