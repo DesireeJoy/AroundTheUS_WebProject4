cb2b990d19e61f3273f877aacb2a39405ba6a23b
DesireeJoy <desireebradish@gmail.com>
2020-12-25 18:17:42 -0500

    add eventlistener for enlarge

--- pages/utils.js
+++ pages/utils.js
@@ -3,7 +3,8 @@ export function enlarge(ele) {
   ele
     .querySelector(".grid__btn_popup")
     .addEventListener("click", function (pic) {
-      openPopUp(popupImageBlock);
+      popupImageBlock.classList.add("popup_visible");
+      document.addEventListener("keydown", closeWithEsc, false);
       popupImage.src = pic.target.src;
       popupCaption.innerHTML = pic.target.alt;
     });
