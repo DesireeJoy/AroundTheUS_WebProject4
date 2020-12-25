d758fce856804b6852233128a4b4b0d353812d37
DesireeJoy <desireebradish@gmail.com>
2020-12-25 18:34:17 -0500

    add eventlistener for enlarge

--- pages/utils.js
+++ pages/utils.js
@@ -1,14 +1,12 @@
+const popupImage = popupImageBlock.querySelector(".grid__image_active");
+const popupCaption = popupImageBlock.querySelector(".popup__image_capt");
 export function enlarge(ele) {
   ele
     .querySelector(".grid__btn_popup")
     .addEventListener("click", function (pic) {
       document.querySelector(".popup__image").classList.add("popup_visible");
       document.addEventListener("keydown", closeWithEsc, false);
-      document
-        .querySelector(".popup__image")
-        .querySelector(".grid__image_active").src = pic.target.src;
-      document
-        .querySelector(".popup__image")
-        .querySelector(".popup__image_capt").innerHTML = pic.target.alt;
+      popupImage.src = pic.target.src;
+      popupCaption.innerHTML = pic.target.alt;
     });
 }
