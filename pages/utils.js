ce73cac41f3881eb8e019cf7c24af7f6bc04a90e
DesireeJoy <desireebradish@gmail.com>
2020-12-25 18:24:15 -0500

    add eventlistener for enlarge

--- pages/utils.js
+++ pages/utils.js
@@ -1,10 +1,3 @@
-export function closeWithEsc(evt, popUpSelect) {
-  if (evt.key === "Escape") {
-    const findCurrent = document.querySelector(".popup_visible");
-    closePopUp(findCurrent);
-  }
-}
-
 export function enlarge(ele) {
   this._templateElement
     .querySelector(".grid__btn_popup")
