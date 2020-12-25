ad607a05870004947aae6876cb968c439a02f9b3
DesireeJoy <desireebradish@gmail.com>
2020-12-25 15:45:13 -0500

    this is a lot

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,54 @@
 "use strict";
-import { enlarge } from "./utils.js";
-import { Card } from "./card.js";
-console.log("update");
+
+class Card {
+  constructor(cardData, templateElement) {
+    // the text and the image are private fields,
+    // they're only needed inside the class
+    this._name = cardData.name;
+    this._link = cardData.link;
+    this._alt = cardData.name;
+    this._templateElement = templateElement;
+  }
+  _getTemplate() {
+    const cardElement = document
+      .querySelector(this._templateElement)
+      .content.querySelector(".grid__card")
+      .cloneNode(true);
+
+    return cardElement;
+  }
+
+  _setEventListeners() {
+    //like
+    this._templateElement
+      .querySelector(".grid__heart")
+      .addEventListener("click", function (evt) {
+        evt.target.classList.toggle("grid__heart_active");
+      });
+
+    //delete
+    this._templateElement
+      .querySelector(".grid__btn_del")
+      .addEventListener("click", function (evt) {
+        const item = evt.target.closest(".grid__card");
+        item.remove();
+      });
+  }
+
+  generateCard() {
+    this._templateElement = this._getTemplate();
+    this._setEventListeners();
+    this._templateElement.querySelector(
+      ".grid__caption"
+    ).textContent = this._name;
+    this._templateElement.querySelector(".grid__image").src = this._link;
+    this._templateElement.querySelector(".grid__image").alt = this._alt;
+    enlarge(this._templateElement);
+
+    return this._templateElement;
+  }
+}
+
 const editBtn = document.querySelector(".profile__editbtn");
 const addBtn = document.querySelector(".profile__addbtn");
 const inputName = document.querySelector("#inputName");
